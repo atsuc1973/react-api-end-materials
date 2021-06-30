80b5ed6be56d391f0e11f4ba7fe7770a9f8843e1
atsuc1973 <atsu1973@gmail.com>
2021-06-30 15:52:01 +0900

    making templates

---
+++ src/components/forms_index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const forms_index = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/forms_second.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const forms_second = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
